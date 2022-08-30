93fecb8baf23970d31e77f4d86b149a28c1952b2
Mike <mvergil123@gmail.com>
2022-08-30 17:51:16 -0600

    Commiting code...

---
+++ WebServer/main.py
@@ -0,0 +1,12 @@
+
+# https://pythonbasics.org/flask-rest-api/
+
+import json
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return json.dumps({'name': 'alice',
+                       'email': 'alice@outlook.com'})
+app.run()
