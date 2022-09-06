da52255bfca72dc45d2df91d0742db40c94effef
keithbecker <keithloganbecker94@gmail.com>
2022-09-06 17:53:28 -0600

    bank

--- WebServer/main.py
+++ WebServer/main.py
@@ -1,36 +1,48 @@
 from flask import request
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 
     file = open("index.html")
-    return file.read()
-
-
-
+    content = file.read()
+    file.close()
+    return content
 
 @app.route('/bank')
 def bank():
     file = open("bank.html")
-    return file.read()
+    content = file.read()
+    file.close()
+    return content
 
 @app.route('/withdrawal')
 def withdrawal():
     pass
 
+
+def get_amount_in_bank():
+    output = os.popen('cat amount.txt')
+    amount_text = int(output.read())
+    return amount_text
+
+
+def save_amount_in_bank(amount):
+    print(amount)
+    os.system(f'echo "1${amount}" > amount.txt')
+
+
 @app.route('/deposit')
 def deposit():
-    with open("amount.txt", "a+") as handler:
-        amount_text = handler.read()
-        print("==============================")
-        print(amount_text)
 
-        amount = int(request.args.get('amount'))
-        amount = amount_text + amount
-        handler.write(str(amount))
-        print(amount)
-        return f"This is the amount ${amount}"
+    save_amount_in_bank("1111111")
+
+    amt = get_amount_in_bank()
+
+    amount = int(request.args.get('amount'))
+
+    return f"This is the amount ${amount}"
 
 app.run()
